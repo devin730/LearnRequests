8315a1c4d85b850feaac72acdfdd07c4f965ce92
Zengming.Deng <deng.zengming@imprexion.com.cn>
2020-03-27 16:17:39 +0800

    add some descriptions.

---
+++ MyDescription.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#coding: utf-8
+
+
+class SplitDescription():
+    def __init__(self, lists):
+        string1 = lists[0]
+        index_director_start = string1.find('导演')
+        index_director_end = string1.find('\xa0')
+        index_stars_start = string1.find('主演')
+        self.Director = string1[index_director_start+3:index_director_end]
+        self.Stars = string1[index_stars_start+3:]
+        string2 = lists[1]
+        # print(string2)
+        Year1, self.Country, self.Category = string2.split('/')[-3:]
+        self.Year = Year1.replace(' ', '')
+        # self.Country = Country1.replace(' ', '')
+        # self.Category = Category1.replace(' ', '')
+
+if __name__ == "__main__":
+    list = ['\n                            导演: 达伦·阿伦诺夫斯基 Darren Aronofsky\xa0\xa0\xa0主演: 艾伦·伯斯汀 Ellen Bur...',
+            '\n                            2000\xa0/\xa0美国\xa0/\xa0剧情\n']
+    x = SplitDescription(list)
--- douban_film_top250.py
+++ douban_film_top250.py
@@ -7,6 +7,7 @@ import time
 import random
 from lxml import etree
 import xlwt
+from MyDescription import SplitDescription
 
 class DoubanFilesTop250():
     def __init__(self):
@@ -21,9 +22,14 @@ class DoubanFilesTop250():
         self.worksheet.write(0, 0, label='排名')
         self.worksheet.write(0, 1, label='电影名')
         self.worksheet.write(0, 2, label='评分')
-        self.worksheet.write(0, 3, label='点评人数')
-        self.worksheet.write(0, 4, label='简介')
-        self.worksheet.write(0, 5, label='网址')
+        self.worksheet.write(0, 3, label='年份')
+        self.worksheet.write(0, 4, label='导演')
+        self.worksheet.write(0, 5, label='明星')
+        self.worksheet.write(0, 6, label='国家')
+        self.worksheet.write(0, 7, label='类型')
+        self.worksheet.write(0, 8, label='评论数')
+        self.worksheet.write(0, 9, label='短评')
+        self.worksheet.write(0, 10, label='网址')
         self.film_count = 1
         for page in range(0, 250, 25):
             url = url_start + str(page)
@@ -33,32 +39,46 @@ class DoubanFilesTop250():
         self.workbook.save('./豆瓣电影TOP250.xls')
 
     def crawl(self, url):
+        print('crawl page....')
         web_data = requests.get(url, headers=self.header).text
         selector = etree.HTML(web_data)
         li_lists = selector.xpath('//ol[@class="grid_view"]/li')
         # print(li_lists)
         for list in li_lists:
             # print(list.xpath('//div[@class="hd"]/a/span[1]/text()'))
-            print('_____________________________________________________')
-            print(list.xpath('div/div[2]/div[@class="hd"]/a/span[1]/text()'))  # title
-            print(list.xpath('div/div[2]/div[@class="hd"]/a/@href'))  # url
-            print(list.xpath('div/div[2]/div[@class="bd"]/p[1]/text()'))  #! description 后期可以处理一下这个文本
-            print(list.xpath('div/div[2]/div[@class="bd"]/div[1]/span[2]/text()'))  # scores
-            print(list.xpath('div/div[2]/div[@class="bd"]/div[1]/span[4]/text()'))  # comments count
-            print(list.xpath('div/div[2]/div[@class="bd"]/p[2]/span/text()'))  # quote
+            # print('_____________________________________________________')
+            # print(list.xpath('div/div[2]/div[@class="hd"]/a/span[1]/text()'))  # title
+            # print(list.xpath('div/div[2]/div[@class="hd"]/a/@href'))  # url
+            # print(list.xpath('div/div[2]/div[@class="bd"]/p[1]/text()'))  #! description 后期可以处理一下这个文本
+            # print(list.xpath('div/div[2]/div[@class="bd"]/div[1]/span[2]/text()'))  # scores
+            # print(list.xpath('div/div[2]/div[@class="bd"]/div[1]/span[4]/text()'))  # comments count
+            # print(list.xpath('div/div[2]/div[@class="bd"]/p[2]/span/text()'))  # quote
 
             title = list.xpath('div/div[2]/div[@class="hd"]/a/span[1]/text()')  # title
             film_url = list.xpath('div/div[2]/div[@class="hd"]/a/@href')  # url
-            # description = list.xpath('div/div[2]/div[@class="bd"]/p[1]/text()')  #! description 后期可以处理一下这个文本
+            description = list.xpath('div/div[2]/div[@class="bd"]/p[1]/text()')  #! description 后期可以处理一下这个文本
             scores = list.xpath('div/div[2]/div[@class="bd"]/div[1]/span[2]/text()')  # scores
             comments_count = list.xpath('div/div[2]/div[@class="bd"]/div[1]/span[4]/text()')  # comments count
             quote = list.xpath('div/div[2]/div[@class="bd"]/p[2]/span/text()')  # quote
+            
+            x = SplitDescription(description)
+            director = x.Director
+            stars = x.Stars
+            year = x.Year
+            country = x.Country
+            category = x.Category
+            
             self.worksheet.write(self.film_count, 0, label=self.film_count)
             self.worksheet.write(self.film_count, 1, label=title)
             self.worksheet.write(self.film_count, 2, label=scores)
-            self.worksheet.write(self.film_count, 3, label=comments_count)
-            self.worksheet.write(self.film_count, 4, label=quote)
-            self.worksheet.write(self.film_count, 5, label=film_url)
+            self.worksheet.write(self.film_count, 3, label=year)
+            self.worksheet.write(self.film_count, 4, label=director)
+            self.worksheet.write(self.film_count, 5, label=stars)
+            self.worksheet.write(self.film_count, 6, label=country)
+            self.worksheet.write(self.film_count, 7, label=category)
+            self.worksheet.write(self.film_count, 8, label=comments_count)
+            self.worksheet.write(self.film_count, 9, label=quote)
+            self.worksheet.write(self.film_count, 10, label=film_url)
             self.film_count = self.film_count + 1
 
 if __name__ == '__main__':
